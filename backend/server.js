2e73afba3f051d2cf034da6b90ec7813f1b58465
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-09-27 09:38:53 +0700

    Create Sign-in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
+dotenv.config();
+
 const app = express();
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/amazona_redux_toolkit",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-  }
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
@@ -24,7 +27,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
