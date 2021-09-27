64279218417e6456095bbb57331c61adc184b9a8
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-09-27 10:56:13 +0700

    Create Place Order API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import orderRouter from "./routers/orderRouter.js";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
